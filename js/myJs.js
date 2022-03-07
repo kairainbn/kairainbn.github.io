47e7f99b625cee1318efab73af2641deec9108e4
kairainbn <101121938+kairainbn@users.noreply.github.com>
2022-03-07 22:51:51 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -36,8 +36,8 @@ $(document).ready(function () {
       title: textConfig.text1,
       text: textConfig.text2,
       imageUrl: "img/soo.gif",
-      imageWidth: 300,
-      imageHeight: 300,
+      imageWidth: 250,
+      imageHeight: 500,
       background: '#fff url("img/iput-bg.jpg")',
       imageAlt: "Custom image",
     }).then(function () {
